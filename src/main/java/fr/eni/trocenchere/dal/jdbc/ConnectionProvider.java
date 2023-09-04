c6874906ef405164f549f8b65b97bc8c7709246e
Thomas MARTIN <thomas.martin2023@campus-eni.fr>
2023-09-04 14:08:15 +0200

    Mise à jour + rajout de la ServletConnexionBDD

--- src/main/java/fr/eni/trocenchere/dal/jdbc/ConnectionProvider.java
+++ src/main/java/fr/eni/trocenchere/dal/jdbc/ConnectionProvider.java
@@ -11,13 +11,8 @@ import javax.sql.DataSource;
 public class ConnectionProvider {
 
 	private static DataSource dataSource;
-	
 	/*
 	 * bloc static :
-	 * bloc de code qui s'éxécute une fois et une seule fois,
-	 * la 1ere fois que l'on appelle la classe (soit par instanciation (new()), 
-	 * soit par appel de méthode static (public), 
-	 * soit par appel d'attribut static (public)).
 	 */
 	static {
 		try {
