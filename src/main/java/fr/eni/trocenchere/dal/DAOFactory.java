ac7ee5bfab63f8b2ae91611354c9116866727ce4
Thomas MARTIN <thomas.martin2023@campus-eni.fr>
2023-09-05 14:19:30 +0200

    DAOFactory avec l'article vendu

--- src/main/java/fr/eni/trocenchere/dal/DAOFactory.java
+++ src/main/java/fr/eni/trocenchere/dal/DAOFactory.java
@@ -1,9 +1,14 @@
 package fr.eni.trocenchere.dal;
 
+import fr.eni.trocenchere.dal.jdbc.ArticleVenduDAOJdbcImpl;
 import fr.eni.trocenchere.dal.jdbc.UtilisateurDAOJdbcImpl;
 
 public class DAOFactory {
 	public static UtilisateurDAO getUtilisateurDAO() {
 		return new UtilisateurDAOJdbcImpl();
+	}
+
+	public static ArticleVenduDAO getArticleVenduDAO() {
+		return new ArticleVenduDAOJdbcImpl();
 	}
 }
