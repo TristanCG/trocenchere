35fb391c2a934104fe524c268dab79c8abff6f6c
tguillou2023 <tguillou2023@129SE23-HC4MMS3.ad.campus-eni.fr>
2023-09-07 10:53:01 +0200

    MAJ Connexion

--- src/main/java/fr/eni/trocenchere/servlets/Categories.java
+++ src/main/java/fr/eni/trocenchere/servlets/Categories.java
@@ -6,6 +6,8 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
 import java.io.IOException;
 import java.util.List;
 
@@ -20,12 +22,21 @@ public class Categories extends HttpServlet {
    
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		List<Categorie> categories = CategorieManager.getInstance().selectAll();
-		request.setAttribute("categories", categories);
 		
-		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/pages/categories.jsp");
-	     rd.forward(request, response);
-	    }
+		HttpSession session = request.getSession(false);
+
+        if (session.getAttribute("noUtilisateur") == null) {
+        	RequestDispatcher rd = request.getRequestDispatcher("accueil");
+		    rd.forward(request, response);
+        } else {
+			List<Categorie> categories = CategorieManager.getInstance().selectAll();
+			request.setAttribute("categories", categories);
+			
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/pages/categories.jsp");
+		    rd.forward(request, response);
+        }
+	    
+	}
 		
 		
 		
--- src/main/java/fr/eni/trocenchere/servlets/Deconnexion.java
+++ src/main/java/fr/eni/trocenchere/servlets/Deconnexion.java
@@ -14,7 +14,7 @@ public class Deconnexion extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		HttpSession session = request.getSession();
+		HttpSession session = request.getSession(false);
 		
 		if (session != null) {
 		    session.invalidate();
