f2f0209d6d92c7a4495d827ab80255dc6223593d
tguillou2023 <tguillou2023@129SE23-HC4MMS3.ad.campus-eni.fr>
2023-09-11 10:52:06 +0200

    test

--- src/main/java/fr/eni/trocenchere/dal/ArticleVenduDAO.java
+++ src/main/java/fr/eni/trocenchere/dal/ArticleVenduDAO.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package fr.eni.trocenchere.dal;
 
 import java.util.List;
@@ -19,25 +18,3 @@ public interface ArticleVenduDAO {
 	Retrait selectRetraitByNoArticle(int noArticle);
 	
 }
-=======
-package fr.eni.trocenchere.dal;
-
-import java.util.List;
-
-import fr.eni.trocenchere.bo.ArticleVendu;
-import fr.eni.trocenchere.bo.Retrait;
-
-public interface ArticleVenduDAO {
-	
-	ArticleVendu insert (ArticleVendu nouveauArticleVendu);
-
-	List<ArticleVendu> selectAll();
-	
-	void insert(Retrait nouveauRetrait);
-
-	ArticleVendu encherir(int noArticle);
-
-	Retrait selectRetraitByNoArticle(int noArticle);
-	 
-}
->>>>>>> branch 'master' of https://github.com/TristanCG/trocenchere.git
